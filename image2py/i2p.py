d41ea4c78f700157f13b275f331b67b1f77453ca
kAlmAcetA <kalmaceta@2147483647.net>
2014-01-10 09:06:59 +0100

    start re-writing

--- image2py/i2p.py
+++ image2py/i2p.py
@@ -12,53 +12,71 @@ except:
 
 class Bare():
 
-    def get_encode(self):
-        return base64.b64encode
+    def __init__(self):
+        self._part_files = ""
+        self._part_headers = ""
+        self._part_functions = ""
+
+    def encode(self, data):
+        return base64.b64encode(data)
+
+    def part_files(self, files):
+        output = "data = {}\n\n"
+        for name, data in files.items():
+            output += "data['{!s}'] = "" \\\n".format(name)
+            while data:
+                part = data[:70]
+                data = data[70:]
+                output += '    "{!s}" \\\n'.format(part)
+        self._part_files = output
+
+    def part_header(self):
+        self.part_headers = "import base64"
+
+    def part_functions(self):
+        self._part_functions = "def get_data(name):\n    return d[name]"
+        self._part_functions += "def get_decoded(name):\n    " \
+                                "return base64.b64decode(d[name])"
 
 
-class Img2Py():
+class Converter(object):
 
-    def __init__(self, files, indent=True, toolkit=None, encode=None,
-                 continue_on_errors=False):
-        self.files = files
-        self.indent = indent
-        if toolkit is None:
-            toolkit = Bare()
+    def __inoit__(self, filename=None):
+        self.output = ""
+        self.files = {}
+        self.filename = filename
+        self.toolkit = Bare()
+        if os.path.isfile(filename) and filename is not None:
+            pass
+
+    def set_toolkit(self, toolkit):
         self.toolkit = toolkit
-        self.coe = continue_on_errors
-        self.encode = encode if encode else toolkit.get_encode()
 
-    def generate(self):
-        self.errors = []
-        self.raw_data = {}
-        for f in self.files:
-            self.process_file(f)
-        return self
+    def save(self, to_file=None):
+        if to_file is None and self.filename is not None:
+            to_file = self.filename
 
-    def output(self, filename=None):
-        output = "hihi"
-        if len(self.raw_data) < 1:
-            raise Exception('No data')
-        if filename is None:
-            return output
-
-    def process_file(self, fname):
-        if not os.path.isfile(fname):
-            self.errors.append("File %s does not exists" % (fname))
-            if self.coe:
-                return
+    def add_file(self, fname):
         with (open(fname, 'rb')) as f:
             data = f.read()
         # below won't handle the same name files
         # in different paths
         filename = os.path.basename(fname)
-        self.raw_data[filename] = self.encode(data)
+        self.files[filename] = self.toolkit.encode(data)
+
+    def remove_file(self, fname):
+        del self.files[fname]
+
+    def output(self):
+        if len(self.files) < 1:
+            raise Exception('No data')
+        return self.output
 
 
 def main():
     #    config = ConfigParser()
     try:
-        o = Img2Py([])
+        o = type()
         print(o.generate().output())
     except Exception as e:
         print("Error: %s" % e)
